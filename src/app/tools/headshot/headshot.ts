57279646b42626912205475be96551018efe25b0
hari <hari@test.com>
2021-02-07 20:59:15 +0530

    correction

--- src/app/tools/headshot/headshot.ts
+++ src/app/tools/headshot/headshot.ts
@@ -1,20 +1,29 @@
 import { AppBridgeService } from "../service/app-bridge.service";
 import {NovoElementProviders, NovoElementsModule} from 'novo-elements';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Util } from '../../util/util';
 
-public url: string;
-private readonly entityId: number;
+@Component({
+    selector:    'headshot-card',
+    templateUrl: './index.html',
+    providers:  [ NovoModalService ]
+})
 
+export class HeadshotCardComponent implements OnInit {
+    public url: string;
+    private readonly entityId: number;
 
-constructor(
-    private novoModalService: NovoModalService,
-    private appBridgeService: AppBridgeService,
-    private location: Location
-    
-) {
-    this.entityId = this.getBullhornId('EntityID');
-}
+    constructor(
+        private novoModalService: NovoModalService,
+        private appBridgeService: AppBridgeService,
+        private location: Location
+        
+    ) {
+        this.entityId = this.getBullhornId('EntityID');
+    }
 
-    ngOnInit() {
+    ngOnInit():void {
         const url =  this.location.path(true);
         const candidateId = url.search.split('EntityId=')[1];
 
@@ -39,3 +48,4 @@ constructor(
     private getBullhornId(param: string): number {
         return parseInt(this.route.snapshot.queryParamMap.get(param), 10);
     }
+}
